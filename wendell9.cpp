8f78befefe0a1cf2f68c2b66a8c108cc0d07b2e1
davidearaujo-blip <davidearaujo65@gmail.com>
2026-07-09 11:22:46 -0300

    Add files via upload
    atividade em andamento

---
+++ wendell9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int num[8];
+    float med, som;
+    for(int i = 0; i < 8; i++){
+        cout<< i << endl;
+        cout<<"Digite o"<<i+1<<"° número"<<endl;
+        
+    }     
+}
